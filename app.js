7ea165106df62857d675d4c70ddb7846f679f863
Ali Jane <alison.smith@dowjones.com>
2018-06-21 21:17:57 -0400

    created ajax call to NYT search database

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': "53942c1dd12442b68688da148c225abd"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
